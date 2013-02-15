961466efc94f617e031cdac3d8371dd778a7ff7b
El Arabi Rhomari <elarabi@hgcareers.com>
2013-02-15 09:57:31 -0700

    Update application/Views/index.php

--- application/Views/index.php
+++ application/Views/index.php
@@ -1,8 +1,8 @@
 <?php 
-include $this->form;
-if($entries){
+include $this->searchForm;
+if(!empty($this->entries)){
     echo '<dl>'
-    foreach ($entries as $idx => $entry ){
+    foreach ($this->entries as $idx => $entry ){
         printf( '<dt>Row#%s</dt>', $idx);
         echo '<dd>';
         var_dump($entry);
